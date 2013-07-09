7d1738197b8035c9d04ae4f32c35f739f06e1e26
HoloAddict <fred.julian@gmail.com>
2013-07-09 20:18:21 +0200

    add fullscreen feature

--- src/net/fred/feedex/activity/EntryActivity.java
+++ src/net/fred/feedex/activity/EntryActivity.java
@@ -91,12 +91,14 @@ import android.view.View.OnKeyListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
@@ -105,6 +107,7 @@ public class EntryActivity extends Activity {
 	private static final String SAVE_INSTANCE_PROGRESS_VISIBLE = "isProgressVisible";
 	private static final String SAVE_INSTANCE_SCROLL_PERCENTAGE = "scrollPercentage";
 	private static final String SAVE_INSTANCE_ENTRIES_IDS = "entriesIds";
+	private static final String SAVE_INSTANCE_IS_FULLSCREEN = "isFullscreen";
 
 	private static final long ANIM_DURATION = 250;
 	private static final TranslateAnimation SLIDE_IN_RIGHT = generateAnimation(1, 0);
@@ -141,7 +144,8 @@ public class EntryActivity extends Activity {
 
 	private static final String BUTTON_START = "<div style=\"text-align: center\"><input type=\"button\" value=\"";
 	private static final String BUTTON_MIDDLE = "\" onclick=\"";
-	private static final String BUTTON_END = "\" style=\"background-color:" + BUTTON_COLOR + "; color:" + TEXT_COLOR + "; border: none; border-radius:10px; padding: 10px;\"/></div>";
+	private static final String BUTTON_END = "\" style=\"background-color:" + BUTTON_COLOR + "; color:" + TEXT_COLOR
+			+ "; border: none; border-radius:10px; padding: 10px;\"/></div>";
 
 	private static final String LINK_BUTTON_START = "<div style=\"text-align: center; margin-top:12px\"><a href=\"";
 	private static final String LINK_BUTTON_MIDDLE = "\" style=\"background-color:" + BUTTON_COLOR + "; color:" + TEXT_COLOR
@@ -150,7 +154,8 @@ public class EntryActivity extends Activity {
 
 	private static final String IMAGE_ENCLOSURE = "[@]image/";
 
-	private int titlePosition, datePosition, mobilizedHtmlPosition, abstractPosition, linkPosition, feedIdPosition, isFavoritePosition, isReadPosition, enclosurePosition, authorPosition;
+	private int titlePosition, datePosition, mobilizedHtmlPosition, abstractPosition, linkPosition, feedIdPosition, isFavoritePosition,
+			isReadPosition, enclosurePosition, authorPosition;
 
 	private long _id = -1;
 	private long _nextId = -1;
@@ -174,6 +179,8 @@ public class EntryActivity extends Activity {
 	private String link, title, enclosure;
 	private LayoutParams layoutParams;
 	private View backBtn, forwardBtn;
+	private ImageView fullscreenBtn;
+	private boolean mIsFullscreen;
 	private boolean localPictures;
 
 	private boolean mIsProgressVisible = false;
@@ -299,6 +306,7 @@ public class EntryActivity extends Activity {
 
 		backBtn = findViewById(R.id.backBtn);
 		forwardBtn = findViewById(R.id.forwardBtn);
+		fullscreenBtn = (ImageView) findViewById(R.id.fullscreenBtn);
 
 		viewFlipper = (ViewFlipper) findViewById(R.id.content_flipper);
 
@@ -319,6 +327,10 @@ public class EntryActivity extends Activity {
 		mIsProgressVisible = savedInstanceState.getBoolean(SAVE_INSTANCE_PROGRESS_VISIBLE);
 		mScrollPercentage = savedInstanceState.getFloat(SAVE_INSTANCE_SCROLL_PERCENTAGE);
 		setProgressBarIndeterminateVisibility(mIsProgressVisible);
+		mIsFullscreen = savedInstanceState.getBoolean(SAVE_INSTANCE_IS_FULLSCREEN);
+		if (mIsFullscreen) {
+			onClickFullscreenBtn(null);
+		}
 		webView.restoreState(savedInstanceState);
 	}
 
@@ -354,6 +366,7 @@ public class EntryActivity extends Activity {
 		outState.putLongArray(SAVE_INSTANCE_ENTRIES_IDS, mEntriesIds);
 
 		outState.putBoolean(SAVE_INSTANCE_PROGRESS_VISIBLE, mIsProgressVisible);
+		outState.putBoolean(SAVE_INSTANCE_IS_FULLSCREEN, mIsFullscreen);
 
 		float positionTopView = webView.getTop();
 		float contentHeight = webView.getContentHeight();
@@ -413,7 +426,8 @@ public class EntryActivity extends Activity {
 			}
 
 			title = entryCursor.getString(titlePosition);
-			Cursor cursor = getContentResolver().query(FeedColumns.CONTENT_URI(feedId), new String[] { FeedColumns.NAME, FeedColumns.URL }, null, null, null);
+			Cursor cursor = getContentResolver().query(FeedColumns.CONTENT_URI(feedId), new String[] { FeedColumns.NAME, FeedColumns.URL }, null,
+					null, null);
 			if (cursor.moveToFirst()) {
 				setTitle(cursor.isNull(0) ? cursor.getString(1) : cursor.getString(0));
 			} else { // fallback, should not be possible
@@ -423,7 +437,8 @@ public class EntryActivity extends Activity {
 
 			if (canShowIcon) {
 				if (iconBytes == null || iconBytes.length == 0) {
-					Cursor iconCursor = getContentResolver().query(FeedColumns.CONTENT_URI(Integer.toString(feedId)), new String[] { FeedColumns._ID, FeedColumns.ICON }, null, null, null);
+					Cursor iconCursor = getContentResolver().query(FeedColumns.CONTENT_URI(Integer.toString(feedId)),
+							new String[] { FeedColumns._ID, FeedColumns.ICON }, null, null, null);
 
 					if (iconCursor.moveToFirst()) {
 						iconBytes = iconCursor.getBlob(1);
@@ -471,7 +486,8 @@ public class EntryActivity extends Activity {
 			long timestamp = entryCursor.getLong(datePosition);
 			link = entryCursor.getString(linkPosition);
 			enclosure = entryCursor.getString(enclosurePosition);
-			webView.loadDataWithBaseURL(null, generateHtmlContent(title, link, contentText, enclosure, author, timestamp), TEXT_HTML, Constants.UTF8, null);
+			webView.loadDataWithBaseURL(null, generateHtmlContent(title, link, contentText, enclosure, author, timestamp), TEXT_HTML, Constants.UTF8,
+					null);
 		}
 
 		entryCursor.close();
@@ -492,7 +508,8 @@ public class EntryActivity extends Activity {
 		}
 		content.append(TITLE_START).append(link).append(TITLE_MIDDLE).append(title).append(TITLE_END).append(SUBTITLE_START);
 		Date date = new Date(timestamp);
-		StringBuilder dateStringBuilder = new StringBuilder(DateFormat.getDateFormat(this).format(date)).append(' ').append(DateFormat.getTimeFormat(this).format(date));
+		StringBuilder dateStringBuilder = new StringBuilder(DateFormat.getDateFormat(this).format(date)).append(' ').append(
+				DateFormat.getTimeFormat(this).format(date));
 
 		if (author != null && !author.isEmpty()) {
 			dateStringBuilder.append(" &mdash; ").append(author);
@@ -507,7 +524,8 @@ public class EntryActivity extends Activity {
 		content.append(BUTTON_END);
 
 		if (enclosure != null && enclosure.length() > 6 && enclosure.indexOf(IMAGE_ENCLOSURE) == -1) {
-			content.append(BUTTON_START).append(getString(R.string.see_enclosure)).append(BUTTON_MIDDLE).append("injectedJSObject.onClickEnclosure();").append(BUTTON_END);
+			content.append(BUTTON_START).append(getString(R.string.see_enclosure)).append(BUTTON_MIDDLE)
+					.append("injectedJSObject.onClickEnclosure();").append(BUTTON_END);
 		}
 
 		if (link != null && link.length() > 0) {
@@ -591,9 +609,11 @@ public class EntryActivity extends Activity {
 		forwardBtn.setVisibility(View.GONE);
 
 		if (mEntriesIds == null) {
-			Cursor cursor = getContentResolver().query(parentUri, EntryColumns.PROJECTION_ID,
-					PrefsManager.getBoolean(PrefsManager.SHOW_READ, true) || EntryColumns.FAVORITES_CONTENT_URI.equals(parentUri) ? null : EntryColumns.WHERE_UNREAD, null,
-					EntryColumns.DATE + Constants.DB_DESC);
+			Cursor cursor = getContentResolver().query(
+					parentUri,
+					EntryColumns.PROJECTION_ID,
+					PrefsManager.getBoolean(PrefsManager.SHOW_READ, true) || EntryColumns.FAVORITES_CONTENT_URI.equals(parentUri) ? null
+							: EntryColumns.WHERE_UNREAD, null, EntryColumns.DATE + Constants.DB_DESC);
 
 			mEntriesIds = new long[cursor.getCount()];
 			int i = 0;
@@ -703,8 +723,9 @@ public class EntryActivity extends Activity {
 		}
 		case R.id.menu_share: {
 			if (link != null) {
-				startActivity(Intent.createChooser(new Intent(Intent.ACTION_SEND).putExtra(Intent.EXTRA_SUBJECT, title).putExtra(Intent.EXTRA_TEXT, link).setType(Constants.MIMETYPE_TEXT_PLAIN),
-						getString(R.string.menu_share)));
+				startActivity(Intent.createChooser(
+						new Intent(Intent.ACTION_SEND).putExtra(Intent.EXTRA_SUBJECT, title).putExtra(Intent.EXTRA_TEXT, link)
+								.setType(Constants.MIMETYPE_TEXT_PLAIN), getString(R.string.menu_share)));
 			}
 			break;
 		}
@@ -778,6 +799,24 @@ public class EntryActivity extends Activity {
 
 	public void onClickForwardBtn(View view) {
 		nextEntry();
+	}
+
+	public void onClickFullscreenBtn(View view) {
+		if (view != null) {
+			mIsFullscreen = !mIsFullscreen;
+		}
+
+		if (mIsFullscreen) {
+			getActionBar().hide();
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+			fullscreenBtn.setImageResource(R.drawable.return_from_full_screen);
+		} else {
+			getActionBar().show();
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			fullscreenBtn.setImageResource(R.drawable.full_screen);
+		}
 	}
 
 	private class JavaScriptObject {
