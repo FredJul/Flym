7cf1945af94b387468eead2010cd9ebce5f64b7e
HoloAddict <fred.julian@gmail.com>
2013-09-16 03:01:39 +0200

    improve drawer display + improve adapter performances

--- src/net/fred/feedex/adapter/DrawerAdapter.java
+++ src/net/fred/feedex/adapter/DrawerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
@@ -18,6 +19,7 @@ import android.widget.TextView;
 
 import net.fred.feedex.Constants;
 import net.fred.feedex.R;
+import net.fred.feedex.UiUtils;
 import net.fred.feedex.provider.FeedData.EntryColumns;
 import net.fred.feedex.provider.FeedData.FeedColumns;
 
@@ -26,14 +28,26 @@ public class DrawerAdapter extends BaseAdapter implements LoaderManager.LoaderCa
     private static final int POS_ID = 0;
     private static final int POS_NAME = 1;
     private static final int POS_IS_GROUP = 2;
-    private static final int POS_ICON = 3;
-    private static final int POS_UNREAD = 4;
-    private static final int POS_ALL_UNREAD = 5;
-    private static final int POS_FAVORITES_UNREAD = 6;
+    private static final int POS_GROUP_ID = 3;
+    private static final int POS_ICON = 4;
+    private static final int POS_UNREAD = 5;
+    private static final int POS_ALL_UNREAD = 6;
+    private static final int POS_FAVORITES_UNREAD = 7;
+
+    private static final int ITEM_PADDING = UiUtils.dpToPixel(20);
+    private static final int NORMAL_TEXT_COLOR = Color.parseColor("#EEEEEE");
+    private static final int GROUP_TEXT_COLOR = Color.parseColor("#BBBBBB");
 
     private final FragmentActivity mActivity;
     private final LoaderManager mLoaderMgr;
     private Cursor mFeedsCursor;
+
+    private static class ViewHolder {
+        public ImageView iconView;
+        public TextView titleTxt;
+        public TextView unreadTxt;
+        public View separator;
+    }
 
     public DrawerAdapter(FragmentActivity activity, int loaderId) {
         mActivity = activity;
@@ -44,52 +58,67 @@ public class DrawerAdapter extends BaseAdapter implements LoaderManager.LoaderCa
 
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View itemView = null;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.drawer_list_item, parent, false);
+
+            ViewHolder holder = new ViewHolder();
+            holder.iconView = (ImageView) convertView.findViewById(R.id.icon);
+            holder.titleTxt = (TextView) convertView.findViewById(R.id.title);
+            holder.unreadTxt = (TextView) convertView.findViewById(R.id.unread_count);
+            holder.separator = convertView.findViewById(R.id.separator);
+            convertView.setTag(holder);
+        }
+
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+
+        // default init
+        holder.iconView.setImageDrawable(null);
+        holder.titleTxt.setText("");
+        holder.titleTxt.setTextColor(NORMAL_TEXT_COLOR);
+        holder.titleTxt.setAllCaps(false);
+        holder.unreadTxt.setText("");
+        convertView.setPadding(0, 0, 0, 0);
+        holder.separator.setVisibility(View.GONE);
 
         if (position == 0 || position == 1) {
-            itemView = inflater.inflate(R.layout.drawer_list_item, parent, false);
-            TextView txtTitle = (TextView) itemView.findViewById(R.id.title);
-            txtTitle.setText(position == 0 ? R.string.all : R.string.favorites);
-
-            ImageView imgIcon = (ImageView) itemView.findViewById(R.id.icon);
-            imgIcon.setImageResource(position == 0 ? R.drawable.ic_statusbar_rss : R.drawable.dimmed_rating_important);
+            holder.titleTxt.setText(position == 0 ? R.string.all : R.string.favorites);
+            holder.iconView.setImageResource(position == 0 ? R.drawable.ic_statusbar_rss : R.drawable.dimmed_rating_important);
 
             if (mFeedsCursor != null && mFeedsCursor.moveToFirst()) {
                 int unread = mFeedsCursor.getInt(position == 0 ? POS_ALL_UNREAD : POS_FAVORITES_UNREAD);
                 if (unread != 0) {
-                    TextView countFeed = (TextView) itemView.findViewById(R.id.menurow_counter);
-                    countFeed.setText(String.valueOf(unread));
+                    holder.unreadTxt.setText(String.valueOf(unread));
                 }
             }
         } else if (mFeedsCursor != null && mFeedsCursor.moveToPosition(position - 2)) {
-            String name = mFeedsCursor.getString(POS_NAME);
-            boolean isGroup = mFeedsCursor.getInt(POS_IS_GROUP) == 1;
+            holder.titleTxt.setText(mFeedsCursor.getString(POS_NAME));
 
-            if (isGroup) {
-                itemView = inflater.inflate(R.layout.drawer_list_header, parent, false);
-                TextView txtTitle = (TextView) itemView.findViewById(R.id.list_header_title);
-                txtTitle.setText(name);
+            if (mFeedsCursor.getInt(POS_IS_GROUP) == 1) {
+                holder.titleTxt.setTextColor(GROUP_TEXT_COLOR);
+                holder.titleTxt.setAllCaps(true);
+                holder.separator.setVisibility(View.VISIBLE);
             } else {
-                itemView = inflater.inflate(R.layout.drawer_list_item, parent, false);
-                TextView txtTitle = (TextView) itemView.findViewById(R.id.title);
-                txtTitle.setText(name);
-
                 byte[] iconBytes = mFeedsCursor.getBlob(POS_ICON);
                 if (iconBytes != null && iconBytes.length > 0) {
                     Bitmap bitmap = BitmapFactory.decodeByteArray(iconBytes, 0, iconBytes.length);
-                    ImageView imgIcon = (ImageView) itemView.findViewById(R.id.icon);
-                    imgIcon.setImageBitmap(bitmap);
+                    holder.iconView.setImageBitmap(bitmap);
+                } else {
+                    holder.iconView.setImageResource(R.drawable.icon);
                 }
 
                 int unread = mFeedsCursor.getInt(POS_UNREAD);
                 if (unread != 0) {
-                    TextView countFeed = (TextView) itemView.findViewById(R.id.menurow_counter);
-                    countFeed.setText(String.valueOf(unread));
+                    holder.unreadTxt.setText(String.valueOf(unread));
                 }
+            }
+
+            if (!mFeedsCursor.isNull(POS_GROUP_ID)) { // First level
+                convertView.setPadding(ITEM_PADDING, 0, 0, 0);
             }
         }
 
-        return itemView;
+        return convertView;
     }
 
     @Override
@@ -132,7 +161,7 @@ public class DrawerAdapter extends BaseAdapter implements LoaderManager.LoaderCa
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        CursorLoader cursorLoader = new CursorLoader(mActivity, FeedColumns.GROUPED_FEEDS_CONTENT_URI, new String[]{FeedColumns._ID, FeedColumns.NAME, FeedColumns.IS_GROUP, FeedColumns.ICON,
+        CursorLoader cursorLoader = new CursorLoader(mActivity, FeedColumns.GROUPED_FEEDS_CONTENT_URI, new String[]{FeedColumns._ID, FeedColumns.NAME, FeedColumns.IS_GROUP, FeedColumns.GROUP_ID, FeedColumns.ICON,
                 "(SELECT COUNT(*) FROM " + EntryColumns.TABLE_NAME + " WHERE " + EntryColumns.IS_READ + " IS NULL AND " + EntryColumns.FEED_ID + "=" + FeedColumns.TABLE_NAME + "." + FeedColumns._ID + ")",
                 "(SELECT COUNT(*) FROM " + EntryColumns.TABLE_NAME + " WHERE " + EntryColumns.IS_READ + " IS NULL)",
                 "(SELECT COUNT(*) FROM " + EntryColumns.TABLE_NAME + " WHERE " + EntryColumns.IS_READ + " IS NULL AND " + EntryColumns.IS_FAVORITE + Constants.DB_IS_TRUE + ")"}, null, null, null);
