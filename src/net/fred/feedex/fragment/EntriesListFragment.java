13241404cbd7c40c8506277edfd3a7f48bf42cd4
HoloAddict <fred.julian@gmail.com>
2013-09-16 18:21:26 +0200

    fix possible crash

--- src/net/fred/feedex/fragment/EntriesListFragment.java
+++ src/net/fred/feedex/fragment/EntriesListFragment.java
@@ -87,23 +87,24 @@ public class EntriesListFragment extends ListFragment implements LoaderManager.L
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (lv != null) {
+                int position = lv.pointToPosition(Math.round(e1.getX()), Math.round(e1.getY()));
 
-            final int position = lv.pointToPosition(Math.round(e1.getX()), Math.round(e1.getY()));
-
-            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
-                if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH || Math.abs(velocityY) < SWIPE_THRESHOLD_VELOCITY) {
-                    return false;
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
+                    if (Math.abs(e1.getX() - e2.getX()) > SWIPE_MAX_OFF_PATH || Math.abs(velocityY) < SWIPE_THRESHOLD_VELOCITY) {
+                        return false;
+                    }
+                } else {
+                    if (Math.abs(velocityX) < SWIPE_THRESHOLD_VELOCITY) {
+                        return false;
+                    }
+                    if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE) {
+                        mEntriesCursorAdapter.markAsReadOrUnread(mEntriesCursorAdapter.getItemId(position));
+                    } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE) {
+                        mEntriesCursorAdapter.changeFavorite(mEntriesCursorAdapter.getItemId(position));
+                    }
+                    mEntriesCursorAdapter.notifyDataSetChanged();
                 }
-            } else {
-                if (Math.abs(velocityX) < SWIPE_THRESHOLD_VELOCITY) {
-                    return false;
-                }
-                if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE) {
-                    mEntriesCursorAdapter.markAsReadOrUnread(mEntriesCursorAdapter.getItemId(position));
-                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE) {
-                    mEntriesCursorAdapter.changeFavorite(mEntriesCursorAdapter.getItemId(position));
-                }
-                mEntriesCursorAdapter.notifyDataSetChanged();
             }
 
             return super.onFling(e1, e2, velocityX, velocityY);
