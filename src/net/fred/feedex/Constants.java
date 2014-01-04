b3b7741336ecc60bb8c98cab23eaf1264c36817b
HoloAddict <fred.julian@gmail.com>
2014-01-04 14:43:32 +0100

    reduce the throttled time

--- src/net/fred/feedex/Constants.java
+++ src/net/fred/feedex/Constants.java
@@ -78,5 +78,5 @@ public final class Constants {
 
     public static final String MIMETYPE_TEXT_PLAIN = "text/plain";
 
-    public static final int UPDATE_THROTTLE_DELAY = 1000;
+    public static final int UPDATE_THROTTLE_DELAY = 500;
 }
