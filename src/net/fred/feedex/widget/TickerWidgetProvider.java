7800cc5effb4d3dac0e37e6f88528e5f5e5489c8
pelya <x.pelya.x@gmail.com>
2014-02-20 20:12:23 +0200

    Fixed ticker widget not updated, changed it's appearance

--- src/net/fred/feedex/widget/TickerWidgetProvider.java
+++ src/net/fred/feedex/widget/TickerWidgetProvider.java
@@ -22,17 +22,15 @@ package net.fred.feedex.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.net.Uri;
 import android.widget.RemoteViews;
 
-import net.fred.feedex.Constants;
 import net.fred.feedex.R;
 import net.fred.feedex.activity.HomeActivity;
 import net.fred.feedex.provider.FeedData;
-import net.fred.feedex.utils.PrefUtils;
 
 public class TickerWidgetProvider extends AppWidgetProvider {
 
@@ -43,41 +41,30 @@ public class TickerWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        RemoteViews widget = createWidgetView(context);
         for (int appWidgetId : appWidgetIds) {
-            Intent svcIntent = new Intent(context, WidgetService.class);
-
-            svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            //svcIntent.putExtra(AppWidgetManager.EXTRA_CUSTOM_INFO, PrefUtils.getInt(appWidgetId + ".fontsize", 0));
-            svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
-
-            RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.tickerwidget);
-            widget.setOnClickPendingIntent(R.id.feed_icon, PendingIntent.getActivity(context, 0, new Intent(context, HomeActivity.class), 0));
-            widget.setOnClickPendingIntent(R.id.feed_ticker, PendingIntent.getActivity(context, 0, new Intent(context, HomeActivity.class), 0));
-            widget.setOnClickPendingIntent(R.id.feed_ticker_background, PendingIntent.getActivity(context, 0, new Intent(context, HomeActivity.class), 0));
-
-            Cursor unread = context.getContentResolver().query(FeedData.EntryColumns.CONTENT_URI, new String[]{ALL_UNREAD_NUMBER}, null, null, null);
-            if (unread != null) {
-                if (unread.moveToFirst()) {
-                    widget.setTextViewText(R.id.feed_ticker, String.valueOf(unread.getInt(0)));
-                }
-                unread.close();
-            }
-
             appWidgetManager.updateAppWidget(appWidgetId, widget);
         }
-
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     public static void updateWidget(Context context) {
-        // Now we need to update the widget
-        //System.out.println("updateWidget ticker widget");
-        Intent intent = new Intent(context, TickerWidgetProvider.class);
-        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        try {
-            pendingIntent.send();
-        } catch (PendingIntent.CanceledException ignored) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        appWidgetManager.updateAppWidget(new ComponentName(context.getPackageName(), TickerWidgetProvider.class.getName()), createWidgetView(context));
+    }
+
+    static RemoteViews createWidgetView(Context context) {
+        RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.ticker_widget);
+        widget.setOnClickPendingIntent(R.id.feed_ticker_tap_area, PendingIntent.getActivity(context, 0, new Intent(context, HomeActivity.class), 0));
+
+        Cursor unread = context.getContentResolver().query(FeedData.EntryColumns.CONTENT_URI, new String[]{ALL_UNREAD_NUMBER}, null, null, null);
+        if (unread != null) {
+            if (unread.moveToFirst()) {
+                widget.setTextViewText(R.id.feed_ticker, String.valueOf(unread.getInt(0)));
+            }
+            unread.close();
         }
+
+        return widget;
     }
 }
