b4517df8a780a192bd7ae49cd6377a0c8e80253e
HoloAddict <fred.julian@gmail.com>
2014-11-11 17:55:41 +0100

    fix possible crash

--- src/net/fred/feedex/activity/BaseActivity.java
+++ src/net/fred/feedex/activity/BaseActivity.java
@@ -125,7 +125,9 @@ public abstract class BaseActivity extends ActionBarActivity {
     public void setImmersiveFullScreen(boolean fullScreen) {
         if (fullScreen) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                getSupportActionBar().hide();
+                if (getSupportActionBar() != null) {
+                    getSupportActionBar().hide();
+                }
                 mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
@@ -134,7 +136,9 @@ public abstract class BaseActivity extends ActionBarActivity {
             }
         } else {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                getSupportActionBar().show();
+                if (getSupportActionBar() != null) {
+                    getSupportActionBar().show();
+                }
                 mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
             } else {
                 setNormalFullScreen(false, true);
@@ -146,7 +150,9 @@ public abstract class BaseActivity extends ActionBarActivity {
         if (fullScreen) {
             mIsNormalFullScreen = true;
 
-            getSupportActionBar().hide();
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().hide();
+            }
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
@@ -156,7 +162,9 @@ public abstract class BaseActivity extends ActionBarActivity {
         } else {
             mIsNormalFullScreen = false;
 
-            getSupportActionBar().show();
+            if (getSupportActionBar() != null) {
+                getSupportActionBar().show();
+            }
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
