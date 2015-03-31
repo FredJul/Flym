7892c332fcd148c7e6f3f55a8554599b2037cbe5
HoloAddict <fred.julian@gmail.com>
2015-03-31 03:24:37 +0200

    first "working" wear app

--- mobile/src/main/java/net/fred/feedex/wear/SendDataService.java
+++ mobile/src/main/java/net/fred/feedex/wear/SendDataService.java
@@ -1,116 +1,93 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * Flym
+ * <p/>
+ * Copyright (c) 2012-2015 Frederic Julian
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package net.fred.feedex.wear;
 
+import android.database.Cursor;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.wearable.DataEvent;
-import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
 
-import java.util.concurrent.TimeUnit;
+import net.fred.feedex.provider.FeedData;
 
-/**
- * Listens for disconnection from home device.
- */
 public class SendDataService extends WearableListenerService {
 
     private static final String TAG = SendDataService.class.getSimpleName();
-    private static final long CONNECTION_TIME_OUT_MS = 1000;
 
-    GoogleApiClient mGoogleApiClient;
+    private static final String COUNT_PATH = "/count";
+    private static final String DATE_KEY = "date";
+    private static final String COUNT_KEY = "count";
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "onCreate");
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Wearable.API)
                 .build();
     }
 
     @Override
-    public void onPeerConnected(Node peer) {
-        super.onPeerConnected(peer);
-        Log.d(TAG, "onPeerConnected");
-    }
-
-    @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         super.onMessageReceived(messageEvent);
+        Log.d(TAG, "onMessageReceived " + messageEvent);
 
-        sendStartMessage();
-    }
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(COUNT_PATH);
+        putDataMapRequest.getDataMap().putLong(DATE_KEY, SystemClock.elapsedRealtime());
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    public void onDataChanged(DataEventBuffer dataEvents) {
-        if (Log.isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, "onDataChanged: " + dataEvents + " for " + getPackageName());
-        }
-        for (DataEvent event : dataEvents) {
-            if (event.getType() == DataEvent.TYPE_DELETED) {
-                Log.i(TAG, event + " deleted");
-            } else if (event.getType() == DataEvent.TYPE_CHANGED) {
-                mGoogleApiClient.blockingConnect(CONNECTION_TIME_OUT_MS, TimeUnit.MILLISECONDS);
-
-                if (mGoogleApiClient.isConnected()) {
-                    // Set the alarm off by default.
-                    boolean alarmOn = false;
-
-                    // Use alarmOn boolean to update the DataItem - phone will respond accordingly
-                    // when it receives the change.
-                    PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/answer");
-                    putDataMapRequest.getDataMap().putBoolean("answer", alarmOn);
-                    Wearable.DataApi.putDataItem(mGoogleApiClient, putDataMapRequest.asPutDataRequest())
-                            .await();
-                } else {
-                    Log.e(TAG, "Failed to toggle alarm on phone - Client disconnected from Google Play "
-                            + "Services");
-                }
-                mGoogleApiClient.disconnect();
+        long nbUnread = 0;
+        Cursor unread = getContentResolver().query(FeedData.EntryColumns.CONTENT_URI, new String[]{FeedData.ALL_UNREAD_NUMBER}, null, null, null);
+        if (unread != null) {
+            if (unread.moveToFirst()) {
+                nbUnread = unread.getLong(0);
             }
+            unread.close();
         }
-        dataEvents.close();
-    }
+        putDataMapRequest.getDataMap().putLong(COUNT_KEY, nbUnread);
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
 
-    private void sendStartMessage() {
-        NodeApi.GetConnectedNodesResult rawNodes =
-                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
-        for (final Node node : rawNodes.getNodes()) {
-            Log.v(TAG, "Node: " + node.getId());
-            PendingResult<MessageApi.SendMessageResult> result = Wearable.MessageApi.sendMessage(
-                    mGoogleApiClient,
-                    node.getId(),
-                    "/data",
-                    null
-            );
+        mGoogleApiClient.blockingConnect();
+
+        if (!mGoogleApiClient.isConnected()) {
+            Log.e(TAG, "Cannot send data, not connected");
+            return;
         }
+
+        Log.d(TAG, "Generating DataItem: " + request);
+        Wearable.DataApi.putDataItem(mGoogleApiClient, request)
+                .setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+                    @Override
+                    public void onResult(DataApi.DataItemResult dataItemResult) {
+                        if (!dataItemResult.getStatus().isSuccess()) {
+                            Log.e(TAG, "ERROR: failed to putDataItem, status code: " + dataItemResult.getStatus().getStatusCode());
+                        }
+                    }
+                });
     }
 }
