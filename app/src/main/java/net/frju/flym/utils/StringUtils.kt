830864f03ec48fed23cb5b6736a60732c0e34e2e
Frederic Julian <f.julian@oodrive.com>
2017-10-06 17:29:23 +0200

    use okio for sha1

--- app/src/main/java/net/frju/flym/utils/StringUtils.kt
+++ app/src/main/java/net/frju/flym/utils/StringUtils.kt
@@ -1,14 +1,8 @@
 package net.frju.flym.utils
 
-import java.math.BigInteger
-import java.security.MessageDigest
+import okio.ByteString
 
 val FILE_SCHEME = "file://"
 val UTF8 = "UTF-8"
-val SHA1_INSTANCE: MessageDigest = MessageDigest.getInstance("SHA-1")
 
-fun String.sha1(): String {
-    val messageDigest = SHA1_INSTANCE.digest(toByteArray())
-    val number = BigInteger(1, messageDigest)
-    return number.toString(16)
-}
+fun String.sha1(): String = ByteString.of(*this.toByteArray()).sha1().hex()
