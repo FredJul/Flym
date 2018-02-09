517e9989cf4ffdbb807bc51c7b013e8f43df29dd
Frédéric Julian <fred.julian@gmail.com>
2018-02-09 12:13:15 +0100

    limit glide cache size to 50MB

--- app/src/main/java/net/frju/flym/MyAppGlideModule.kt
+++ app/src/main/java/net/frju/flym/MyAppGlideModule.kt
@@ -1,7 +1,15 @@
 package net.frju.flym
 
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.module.AppGlideModule
 
 @GlideModule
-class MyAppGlideModule : AppGlideModule()
+class MyAppGlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context?, builder: GlideBuilder?) {
+        val diskCacheSizeBytes = 1024 * 1024 * 50 // 50 MB
+        builder?.setDiskCache(InternalCacheDiskCacheFactory(context, diskCacheSizeBytes.toLong()))
+    }
+}
