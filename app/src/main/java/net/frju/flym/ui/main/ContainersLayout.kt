5d899ad24a423cdf8c3128036d374a91fbdf46f5
Frederic Julian <f.julian@oodrive.com>
2018-04-16 13:58:39 +0200

    better kotlin code

--- app/src/main/java/net/frju/flym/ui/main/ContainersLayout.kt
+++ app/src/main/java/net/frju/flym/ui/main/ContainersLayout.kt
@@ -15,145 +15,154 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.view_main_containers.view.*
 import net.fred.feedex.R
 import net.frju.flym.utils.onLaidOut
 
 class ContainersLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
 
-    companion object {
+	companion object {
 
-        const val ANIM_DURATION = 250
+		const val ANIM_DURATION = 250
 
-        private const val STATE_SUPER = "state_super"
-        private const val STATE_CONTAINERS_STATE = "state_containers_state"
-    }
+		private const val STATE_SUPER = "state_super"
+		private const val STATE_CONTAINERS_STATE = "state_containers_state"
+	}
 
-    var state: MainNavigator.State? = null
-        set(state) {
-            field = state
-            when (state) {
-                MainNavigator.State.SINGLE_COLUMN_MASTER -> singleColumnMaster()
-                MainNavigator.State.SINGLE_COLUMN_DETAILS -> singleColumnDetails()
-                MainNavigator.State.TWO_COLUMNS_EMPTY -> twoColumnsEmpty()
-                MainNavigator.State.TWO_COLUMNS_WITH_DETAILS -> twoColumnsWithDetails()
-            }
-        }
+	var state: MainNavigator.State? = null
+		set(state) {
+			field = state
+			when (state) {
+				MainNavigator.State.SINGLE_COLUMN_MASTER -> singleColumnMaster()
+				MainNavigator.State.SINGLE_COLUMN_DETAILS -> singleColumnDetails()
+				MainNavigator.State.TWO_COLUMNS_EMPTY -> twoColumnsEmpty()
+				MainNavigator.State.TWO_COLUMNS_WITH_DETAILS -> twoColumnsWithDetails()
+			}
+		}
 
-    init {
-        LayoutInflater.from(context).inflate(R.layout.view_main_containers, this, true)
-    }
+	init {
+		LayoutInflater.from(context).inflate(R.layout.view_main_containers, this, true)
+	}
 
-    fun hasTwoColumns(): Boolean {
-        return two_columns_container != null
-    }
+	fun hasTwoColumns(): Boolean {
+		return two_columns_container != null
+	}
 
-    private fun singleColumnMaster() {
-        if (hasTwoColumns()) {
-            frame_details.visibility = View.GONE
-            toolbar.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
-            toolbar.layoutParams = toolbar.layoutParams
-            frame_master.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
-            frame_master.layoutParams = frame_master.layoutParams
-        } else {
-            animateOutFrameDetails()
-        }
-        frame_master.visibility = View.VISIBLE
-    }
+	private fun singleColumnMaster() {
+		if (hasTwoColumns()) {
+			frame_details.visibility = View.GONE
+			toolbar.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+			toolbar.layoutParams = toolbar.layoutParams
+			frame_master.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
+			frame_master.layoutParams = frame_master.layoutParams
+		} else {
+			animateOutFrameDetails()
+		}
+		frame_master.visibility = View.VISIBLE
+	}
 
-    private fun singleColumnDetails() {
-        frame_master.visibility = View.GONE
-        frame_details.visibility = View.VISIBLE
-    }
+	private fun singleColumnDetails() {
+		frame_master.visibility = View.GONE
+		frame_details.visibility = View.VISIBLE
+	}
 
-    private fun twoColumnsEmpty() {
-        frame_master.visibility = View.VISIBLE
-        if (hasTwoColumns()) {
-            val res = frame_master.context.resources
-            toolbar.layoutParams.width = res.getDimensionPixelSize(R.dimen.container_max_width)
-            toolbar.layoutParams = toolbar.layoutParams
-            frame_master.layoutParams.width = res.getDimensionPixelSize(R.dimen.container_max_width)
-            frame_master.layoutParams = frame_master.layoutParams
-            frame_details.visibility = View.VISIBLE
-            (frame_details.layoutParams as FrameLayout.LayoutParams).marginStart = res.getDimensionPixelSize(R.dimen.container_max_width)
-            frame_details.layoutParams = frame_details.layoutParams
-        } else {
-            animateOutFrameDetails()
-        }
-    }
+	private fun twoColumnsEmpty() {
+		if (hasTwoColumns()) {
+			setupSecondColumn()
+		} else {
+			animateOutFrameDetails()
+		}
+	}
 
-    private fun twoColumnsWithDetails() {
-        if (hasTwoColumns()) {
-            frame_master.visibility = View.VISIBLE
-            val res = frame_master.context.resources
-            toolbar.layoutParams.width = res.getDimensionPixelSize(R.dimen.container_max_width)
-            toolbar.layoutParams = toolbar.layoutParams
-            frame_master.layoutParams.width = res.getDimensionPixelSize(R.dimen.container_max_width)
-            frame_master.layoutParams = frame_master.layoutParams
-            frame_details.visibility = View.VISIBLE
-            (frame_details.layoutParams as FrameLayout.LayoutParams).marginStart = res.getDimensionPixelSize(R.dimen.container_max_width)
-            frame_details.layoutParams = frame_details.layoutParams
-        } else {
-            animateInFrameDetails()
-        }
-    }
+	private fun twoColumnsWithDetails() {
+		if (hasTwoColumns()) {
+			setupSecondColumn()
+		} else {
+			animateInFrameDetails()
+		}
+	}
 
-    private fun animateInFrameDetails() {
-        frame_details.visibility = View.VISIBLE
-        frame_details.onLaidOut {
-            val alpha = ObjectAnimator.ofFloat(frame_details, View.ALPHA, 0.4f, 1f)
-            val translate = ofFloat(frame_details, View.TRANSLATION_Y, frame_details.height * 0.3f, 0f)
+	private fun setupSecondColumn() {
+		toolbar.run {
+			layoutParams.width = context.resources.getDimensionPixelSize(R.dimen.container_max_width)
+			layoutParams = layoutParams
+		}
 
-            val set = AnimatorSet()
-            set.playTogether(alpha, translate)
-            set.duration = ANIM_DURATION.toLong()
-            set.interpolator = LinearOutSlowInInterpolator()
-            set.addListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator) {
-                    super.onAnimationEnd(animation)
-                    frame_master.visibility = View.GONE
-                }
-            })
-            set.start()
-        }
-    }
+		frame_master.run {
+			isVisible = true
+			layoutParams.width = context.resources.getDimensionPixelSize(R.dimen.container_max_width)
+			layoutParams = frame_master.layoutParams
+		}
+		frame_details.run {
+			isVisible = true
+			(layoutParams as LayoutParams).marginStart = context.resources.getDimensionPixelSize(R.dimen.container_max_width)
+			layoutParams = frame_details.layoutParams
+		}
+	}
 
-    private fun animateOutFrameDetails() {
-        frame_details.onLaidOut {
-            if (frame_details.isShown) {
-                val alpha = ObjectAnimator.ofFloat(frame_details, View.ALPHA, 1f, 0f)
-                val translate = ofFloat(frame_details, View.TRANSLATION_Y, 0f, frame_details.height * 0.3f)
+	private fun animateInFrameDetails() {
+		frame_master.isVisible = true
+		frame_details.isVisible = true
 
-                val set = AnimatorSet()
-                set.playTogether(alpha, translate)
-                set.duration = ANIM_DURATION.toLong()
-                set.interpolator = FastOutLinearInInterpolator()
-                set.addListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator) {
-                        super.onAnimationEnd(animation)
-                        frame_details.alpha = 1f
-                        frame_details.translationY = 0f
-                        frame_details.visibility = View.GONE
-                    }
-                })
-                set.start()
-            }
-        }
-    }
+		frame_details.onLaidOut {
+			val alpha = ObjectAnimator.ofFloat(frame_details, View.ALPHA, 0.4f, 1f)
+			val translate = ofFloat(frame_details, View.TRANSLATION_Y, frame_details.height * 0.3f, 0f)
 
-    override fun onSaveInstanceState(): Parcelable {
-        val bundle = Bundle()
-        bundle.putParcelable(STATE_SUPER, super.onSaveInstanceState())
-        bundle.putString(STATE_CONTAINERS_STATE, state?.name)
-        return bundle
-    }
+			AnimatorSet().run {
+				playTogether(alpha, translate)
+				duration = ANIM_DURATION.toLong()
+				interpolator = LinearOutSlowInInterpolator()
+				addListener(object : AnimatorListenerAdapter() {
+					override fun onAnimationEnd(animation: Animator) {
+						super.onAnimationEnd(animation)
+						frame_master.isGone = true
+					}
+				})
+				start()
+			}
+		}
+	}
 
-    public override fun onRestoreInstanceState(parcelable: Parcelable) {
-        var superParcelable = parcelable
-        if (parcelable is Bundle) {
-            state = MainNavigator.State.valueOf(parcelable.getString(STATE_CONTAINERS_STATE))
-            superParcelable = parcelable.getParcelable<Parcelable>(STATE_SUPER)
-        }
-        super.onRestoreInstanceState(superParcelable)
-    }
+	private fun animateOutFrameDetails() {
+		frame_master.isVisible = true
+		frame_details.onLaidOut {
+			if (frame_details.isShown) {
+				val alpha = ObjectAnimator.ofFloat(frame_details, View.ALPHA, 1f, 0f)
+				val translate = ofFloat(frame_details, View.TRANSLATION_Y, 0f, frame_details.height * 0.3f)
+
+				AnimatorSet().run {
+					playTogether(alpha, translate)
+					duration = ANIM_DURATION.toLong()
+					interpolator = FastOutLinearInInterpolator()
+					addListener(object : AnimatorListenerAdapter() {
+						override fun onAnimationEnd(animation: Animator) {
+							super.onAnimationEnd(animation)
+							frame_details.alpha = 1f
+							frame_details.translationY = 0f
+							frame_details.isGone = true
+						}
+					})
+					start()
+				}
+			}
+		}
+	}
+
+	override fun onSaveInstanceState(): Parcelable {
+		val bundle = Bundle()
+		bundle.putParcelable(STATE_SUPER, super.onSaveInstanceState())
+		bundle.putString(STATE_CONTAINERS_STATE, state?.name)
+		return bundle
+	}
+
+	public override fun onRestoreInstanceState(parcelable: Parcelable) {
+		var superParcelable = parcelable
+		if (parcelable is Bundle) {
+			state = MainNavigator.State.valueOf(parcelable.getString(STATE_CONTAINERS_STATE))
+			superParcelable = parcelable.getParcelable<Parcelable>(STATE_SUPER)
+		}
+		super.onRestoreInstanceState(superParcelable)
+	}
 }
