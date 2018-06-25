682acd6d88e64c17b8c74a8a136e1fdb5ae5ea39
Frédéric Julian <fred.julian@gmail.com>
2018-06-25 23:28:40 +0200

    fix display when srcset images

--- app/src/main/java/net/frju/flym/utils/HtmlUtils.kt
+++ app/src/main/java/net/frju/flym/utils/HtmlUtils.kt
@@ -41,15 +41,16 @@ object HtmlUtils {
 
     private val IMG_PATTERN = Pattern.compile("<img\\s+[^>]*src=\\s*['\"]([^'\"]+)['\"][^>]*>", Pattern.CASE_INSENSITIVE)
     private val ADS_PATTERN = Pattern.compile("<div class=('|\")mf-viral('|\")><table border=('|\")0('|\")>.*", Pattern.CASE_INSENSITIVE)
+    private val SRCSET_PATTERN = Pattern.compile("\\s+srcset=\\s*['\"]([^'\"\\s]+)[^'\"]*['\"]", Pattern.CASE_INSENSITIVE)
     private val LAZY_LOADING_PATTERN = Pattern.compile("\\s+src=[^>]+\\s+(original|data)[-]*src=(\"|')", Pattern.CASE_INSENSITIVE)
-    private val EMPTY_IMAGE_PATTERN = Pattern.compile("<img\\s+(height=['\"]1['\"]\\s+width=['\"]1['\"]|width=['\"]1['\"]\\s+height=['\"]1['\"])\\s+[^>]*src=\\s*['\"]([^'\"]+)['\"][^>]*>", Pattern.CASE_INSENSITIVE)
+    private val PIXEL_IMAGE_PATTERN = Pattern.compile("<img\\s+(height=['\"]1['\"]\\s+width=['\"]1['\"]|width=['\"]1['\"]\\s+height=['\"]1['\"])\\s+[^>]*src=\\s*['\"]([^'\"]+)['\"][^>]*>", Pattern.CASE_INSENSITIVE)
     private val NON_HTTP_IMAGE_PATTERN = Pattern.compile("\\s+(href|src)=(\"|')//", Pattern.CASE_INSENSITIVE)
     private val BAD_IMAGE_PATTERN = Pattern.compile("<img\\s+[^>]*src=\\s*['\"]([^'\"]+)\\.img['\"][^>]*>", Pattern.CASE_INSENSITIVE)
+    private val EMPTY_IMAGE_PATTERN = Pattern.compile("<img((?!src=).)*?>", Pattern.CASE_INSENSITIVE)
     private val START_BR_PATTERN = Pattern.compile("^(\\s*<br\\s*[/]*>\\s*)*", Pattern.CASE_INSENSITIVE)
     private val END_BR_PATTERN = Pattern.compile("(\\s*<br\\s*[/]*>\\s*)*$", Pattern.CASE_INSENSITIVE)
     private val MULTIPLE_BR_PATTERN = Pattern.compile("(\\s*<br\\s*[/]*>\\s*){3,}", Pattern.CASE_INSENSITIVE)
     private val EMPTY_LINK_PATTERN = Pattern.compile("<a\\s+[^>]*></a>", Pattern.CASE_INSENSITIVE)
-
 
     fun improveHtmlContent(content: String, baseUri: String): String {
         @Suppress("NAME_SHADOWING")
@@ -57,6 +58,8 @@ object HtmlUtils {
 
         // remove some ads
         content = ADS_PATTERN.matcher(content).replaceAll("")
+        // take the first image in srcset links
+        content = SRCSET_PATTERN.matcher(content).replaceAll(" src='$1'")
         // remove lazy loading images stuff
         content = LAZY_LOADING_PATTERN.matcher(content).replaceAll(" src=$2")
 
@@ -64,8 +67,9 @@ object HtmlUtils {
         content = Jsoup.clean(content, baseUri, JSOUP_WHITELIST)
 
         // remove empty or bad images
-        content = EMPTY_IMAGE_PATTERN.matcher(content).replaceAll("")
+        content = PIXEL_IMAGE_PATTERN.matcher(content).replaceAll("")
         content = BAD_IMAGE_PATTERN.matcher(content).replaceAll("")
+        content = EMPTY_IMAGE_PATTERN.matcher(content).replaceAll("")
         // remove empty links
         content = EMPTY_LINK_PATTERN.matcher(content).replaceAll("")
         // fix non http image paths
